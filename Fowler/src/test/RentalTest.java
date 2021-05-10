cf33ed640a7fc8a00013b593d648f3d2ae11481f
sarah1410 <sarah.glatt1@gmail.com>
2021-05-10 17:25:28 +0200

    JUnit Test Rental

---
+++ Fowler/src/test/RentalTest.java
@@ -0,0 +1,29 @@
+package test;
+
+import main.Movie;
+import main.Rental;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class RentalTest {
+
+    public static Rental rental;
+    public static Movie movie;
+
+    @BeforeAll
+    public static void setUp() {
+        movie = new Movie("Movie", 1);
+        rental = new Rental(movie, 70);
+    }
+
+    @Test
+    void getDaysRented() {
+        Assertions.assertEquals(70, rental.getDaysRented());
+    }
+
+    @Test
+    void getMovie() {
+        Assertions.assertEquals(movie, rental.getMovie());
+    }
+}
