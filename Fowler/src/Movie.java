34d9c9b7b5e79f6207b968f47b5ef334235e156b
sarah1410 <sarah.glatt1@gmail.com>
2021-05-10 16:37:18 +0200

    Fixed Code Movie

--- Fowler/src/Movie.java
+++ Fowler/src/Movie.java
@@ -1,48 +1,49 @@
 public class Movie {
+
     public static final int CHILDRENS = 2;
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
+
     private String title;
     private int priceCode;
     private Price price;
 
-    public Movie(String newtitle, int priceCode) {
-        title = newtitle;
-        setPriceCode(priceCode);
+    public Movie(String title, int priceCode) {
+        this.title = title;
+        this.setPriceCode(priceCode);
     }
 
     public int getPriceCode() {
-        return price.getPriceCode();
+        return this.price.getPriceCode();
     }
 
-    public void setPriceCode(int arg) {
-        switch (arg) {
+    public void setPriceCode(int priceCode) {
+        switch (priceCode) {
             case REGULAR:
-                price = new RegularPrice();
+                this.price = new RegularPrice();
                 break;
             case CHILDRENS:
-                price = new ChildrensPrice();
+                this.price = new ChildrensPrice();
                 break;
             case NEW_RELEASE:
-                price = new NewReleasePrice();
+                this.price = new NewReleasePrice();
                 break;
             default:
                 throw new IllegalArgumentException("Incorrect Price Code");
         }
     }
 
-    public String getTitle (){
-        return title;
+    public String getTitle() {
+        return this.title;
     }
 
     public double getCharge(int daysRented) {
-        return price.getCharge(daysRented);
+        return this.price.getCharge(daysRented);
     }
 
-    public int getFrequentRenterPoints( int daysRented) {
-        return price.getFrequentRenterPoints(daysRented);
+    public int getFrequentRenterPoints(int daysRented) {
+        return this.price.getFrequentRenterPoints(daysRented);
 
     }
-
 
 }
